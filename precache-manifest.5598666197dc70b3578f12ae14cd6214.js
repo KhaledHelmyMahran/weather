0e7a9ae4558ff20ac36ae7e41078b30fb0ae062b
KhaledHelmyMahran <khaledhelmy774@gmail.com>
2020-02-05 09:59:49 +0200

    Updates

---
+++ precache-manifest.5598666197dc70b3578f12ae14cd6214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ad053dea22c4b7bfe5fe6d5c8e22fa",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "d3360369d975e4b1d475",
+    "url": "/weather/static/css/main.b4f4a9c1.chunk.css"
+  },
+  {
+    "revision": "266ce1aa59a8083b9924",
+    "url": "/weather/static/js/2.33e9494c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather/static/js/2.33e9494c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3360369d975e4b1d475",
+    "url": "/weather/static/js/main.8e6e5612.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  }
+]);
